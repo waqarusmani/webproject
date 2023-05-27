a9032598b7b6887dabe97dc0c5d47a6681977a7b
waqarusmani <waqar.hasan.usmani@gmail.com>
2023-05-27 21:39:26 +0500

    folder adjusted

--- src/assets/index.js
+++
@@ -1,7 +0,0 @@
-
-// const profile = require('./images/profile.png')//porana
-import profile from './images/profile.png'//new update
-
-export {
-    profile
-}
--- src/redux/allGlobleStates.js
+++
@@ -1,5 +0,0 @@
-import { combineReducers } from "redux";
-import countReducer from './reducer/countReducer'
-
-
-export default combineReducers({countReducer})
